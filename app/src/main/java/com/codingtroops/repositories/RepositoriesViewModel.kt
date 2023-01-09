97c6878788225bf10de4ac2ff08c0a7abce70ffb
emmanuel awuah <emmanuelawuah.dev@gmail.com>
2023-01-09 06:13:48 +0000

    setup pager

--- app/src/main/java/com/codingtroops/repositories/RepositoriesViewModel.kt
+++ app/src/main/java/com/codingtroops/repositories/RepositoriesViewModel.kt
@@ -3,17 +3,22 @@ package com.codingtroops.repositories
 import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import androidx.paging.Pager
+import androidx.paging.PagingConfig
+import androidx.paging.PagingData
+import androidx.paging.cachedIn
+import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.launch
 
 
 class RepositoriesViewModel(
-    private val restInterface: RepositoriesApiService
-    = DependencyContainer.repositoriesRetrofitClient
+    private val pagingSource: RepositoriesPagingSource =
+        RepositoriesPagingSource()
 ) : ViewModel() {
-    val repositories = mutableStateOf(emptyList<Repository>())
-    init {
-        viewModelScope.launch {
-            repositories.value = restInterface.getRepositories().repos
-        }
-    }
+
+    val repositories: Flow<PagingData<Repository>> = Pager(
+        config = PagingConfig(pageSize = 20),
+        pagingSourceFactory = { pagingSource }
+    ).flow.cachedIn(viewModelScope)
+
 }
